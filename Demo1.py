453fbbcdd6e0d570b3a9ff0ed8b7fcbab65ea52d
mdelafuen <mdelafuentemartinez@student.bridgew.edu>
2020-09-13 10:49:45 -0400

    Initial commit

---
+++ Demo1.py
@@ -0,0 +1,14 @@
+
+def main():
+    print("This is in a function")
+    print("this is the second line")
+    age = 5
+    dog_years = age * 7
+    print(dog_years)
+    fake_age = "29"
+    print(fake_age)
+    updated_fake_age= fake_age * 5
+    print(updated_fake_age)
+    print(10/3)
+
+main()
