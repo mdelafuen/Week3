10109552febff6f28841921ec6ec2c441486fd7b
mdelafuen <mdelafuentemartinez@student.bridgew.edu>
2020-09-15 08:08:36 -0400

    f string demo

--- StringDemo.py
+++ StringDemo.py
@@ -1,9 +1,9 @@
 
 def main():
+    # Now let's find out who you are
+    name = input("What is your name?")
     age = 5
-    str_demo = f"Wow you are {age}, you are growing up"
+    str_demo = f"Wow {name} you are {age}, you are growing up"
     print(str_demo)
-
-
 
 main()
