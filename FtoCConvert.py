8477765ffc53c4587d955e31a39ee97946322915
mdelafuen <mdelafuentemartinez@student.bridgew.edu>
2020-09-15 08:33:27 -0400

    celcius to farenheit

---
+++ FtoCConvert.py
@@ -0,0 +1,11 @@
+
+def main ():
+    temp_in_c = float(input("What is the Celcius temperature?"))
+    temp_in_f = temp_in_c * 1.8 + 32
+    print(f"The Farenheit temperature is {temp_in_f}")
+
+
+
+
+
+main()
